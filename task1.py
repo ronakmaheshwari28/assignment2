ba2def0ccf2f76fbb7160a2931b84ca7110b5531
ronakmaheshwari28 <Ronakmalpani28@gmail.com>
2025-05-17 10:42:00 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter a number: "))
+if n1%2==0:
+    print(f"{n1} is an even number")
+else:
+    print(f"{n1} is an odd number")
+
---
+++ task2.py
@@ -0,0 +1,7 @@
+# Initialize sum
+total_sum = 0
+# Use a for loop to iterate from 1 to 50 (inclusive)
+for number in range(1, 51):
+    total_sum += number  # Add each number to the total sum
+# Display the final sum
+print(f"The sum of numbers from 1 to 50 is: {total_sum}")
